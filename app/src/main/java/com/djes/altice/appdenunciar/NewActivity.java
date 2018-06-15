0352dc719faca53ca9902300a476b6b52804392b
despinosa <darwinjoel13@gmail.com>
2018-06-15 13:11:37 -0400

    New Denuncia

--- app/src/main/java/com/djes/altice/appdenunciar/NewActivity.java
+++ app/src/main/java/com/djes/altice/appdenunciar/NewActivity.java
@@ -1,13 +1,177 @@
 package com.djes.altice.appdenunciar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Camera;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class NewActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private ImageView btnCamera;
+    private Button btnPublicar;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mUbicacion;
+
+    private TextView txtUbicacion;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            btnCamera.setImageBitmap(imageBitmap);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        btnCamera = findViewById(R.id.btnCamera);
+        txtUbicacion =  findViewById(R.id.txtUbicacion);
+
+        btnPublicar = findViewById(R.id.btnPublicar);
+
+        btnPublicar.setOnClickListener(v->{
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+
+            StorageReference denunciaRef = storageRef.child("Denuncia_"+ new Date().getTime());
+
+
+            // Get the data from an ImageView as bytes
+            btnCamera.setDrawingCacheEnabled(true);
+            btnCamera.buildDrawingCache();
+            Bitmap bitmap = ((BitmapDrawable) btnCamera.getDrawable()).getBitmap();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = denunciaRef.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    String PhotoUrl = storageRef.getDownloadUrl().toString();
+                    Double Latitude = mUbicacion.getLatitude();
+                    Double Longitude = mUbicacion.getLongitude();
+
+
+                }
+            });
+
+        });
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED ) {
+
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                Geocoder geocoder = new Geocoder(NewActivity.this, Locale.getDefault());
+                                try {
+                                    mUbicacion = location;
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    Address obj = addresses.get(0);
+                                    txtUbicacion.setText(obj.getSubLocality()+", "+obj.getLocality()+", "+obj.getCountryName());
+                                }
+                                catch(Exception e)
+                                {
+                                      e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+        }
+
+
+
+
+        btnCamera.setOnClickListener(v->{
+            if(checkCameraHardware(NewActivity.this))
+            {
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
+
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
 }
